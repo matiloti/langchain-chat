9333476190c9a0d18e39edf6d90fd1f5a968b1da
Matias Lotito <matiaslotitoralli@gmail.com>
2026-01-23 22:38:19 +0100

    more code

--- agent.py
+++ agent.py
@@ -3,8 +3,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.messages import HumanMessage, AIMessage
+from langchain_community.tools import BraveSearch
+from langchain.agents.middleware import ToolRetryMiddleware
 
 app = FastAPI()
+
+brave_tool = BraveSearch()
 
 class Message(BaseModel):
     role: str
@@ -15,7 +19,30 @@ class Request(BaseModel):
 
 agent = create_agent(
     "gpt-5-nano",
-    system_prompt="You are a helpful assistant"
+    tools=[brave_tool],
+    middleware=[
+        ToolRetryMiddleware(
+            max_retries=100,
+            backoff_factor=2.0,
+            initial_delay=1.0,
+            max_delay=1
+        ),
+    ],
+    system_prompt="""
+        You are a helpful assistant.
+
+        User may ask questions that require web search. In those cases, use the provided brave search tool.
+
+        Don't overcomplicate user queries. Simplify.
+
+        If user asks plainly "for the weather", get temperature in Celsius, rain probability, and humidity.
+
+        Be direct and concise. Do not ask questions, just answer the user.
+
+        The search tool may fail sometimes. Its ok, just keep trying until it succeeds.
+
+        IMPORTANT: SHORT ANSWERS.
+    """
 )
 
 app.add_middleware(
@@ -26,7 +53,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+# run: uvicorn agent:app --reload
 @app.post("/api/chat")
 def chat(req: Request):
     print(req.messages)
--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
 function App() {
 
-  const [ messages, setMessages ] = useState<Message[]>([{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"},{role: "user", content: "lorem"}])
+  const [ messages, setMessages ] = useState<Message[]>([])
   const [ message, setMessage ] = useState<string>("");
   const [ isLoading, setIsLoading ] = useState<boolean>(false);
 
@@ -40,18 +40,18 @@ function App() {
       if (data.success && data.message.length > 0) {
         // Add assistant message to chat
         setMessages(prev => [
+          ...prev,
           {
             content: data.message,
             role: "assistant"
-          },
-          ...prev
+          }
         ]);
       }
     } catch (error) {
       console.error('Error sending message:', error);
       setMessages(prev => [
         ...prev,
-        { content: "Sorry, there was an error processing your message.", role: "assistant" }
+        { content: "Sorry, there was an error processing your message.", role: "system" }
       ]);
     } finally {
       setIsLoading(false);
@@ -68,27 +68,57 @@ function App() {
   }
 
   return (
-      <div className='flex flex-col lg:min-w-200 md:min-w-190 sm:min-w-full min-w-full bg-gray-50 drop-shadow-xl p-10 rounded-2xl h-full'>
-        <div id="papi" className='flex flex-col h-200 overflow-y-auto  '>
+    <div className='flex flex-col items-center bg-linear-to-br from-blue-300 to-red-300 p-5 h-svh '>
+      <div className='flex mb-3 lg:min-w-200 md:min-w-190 sm:min-w-full min-w-full'>
+        <div className='flex-5 bg-gray-50 drop-shadow-xl rounded-md mr-3'>
+          <div className='p-5'>
+            <span className='block font-bold text-yellow-600 italic text-2xl'>Hello! ✨</span>
+            <span className='block'>I'm  <span className='font-bold'>Matias</span>, and I've done this little chat app to practice creating a fullstack app with agents.</span>
+            <span className='block'>It's simple but the agent has internet access, so you can chat with it and ask for online info.</span>
+            <span className='block'>Emojis make it seems like I vibecoded it but I did not if you see the code repo is all spaguetti AI would do much better.</span>
+          </div>
+        </div>
+        <div className='flex-2 drop-shadow-xl rounded-md bg-blue-50'>
+          <div className='p-5'>
+            <div className='font-bold text-center'>Tech Stack 💪</div>
+            </div>
+        </div>
+      </div>
+      <div className='flex flex-col flex-1 justify-end lg:min-w-200 md:min-w-190 sm:min-w-full min-w-full bg-gray-50 drop-shadow-xl rounded-md min-h-0'>
+        <div className='overflow-y-auto flex flex-col'>
         {
           messages.map(msg => (
-            <div className={(msg.role === "user" ? "bg-linear-to-br from-green-200 to-green-300 self-end max-w-100 " : "bg-linear-to-br from-blue-200 to-purple-300 max-w-100 ") + " mb-5 rounded-lg p-4 drop-shadow max-w-5"}>{msg.content}</div>
+            <div className={
+              (
+                (msg.role === "user") ? "bg-linear-to-br from-green-200 to-green-300 self-end " 
+                : (msg.role === "assistant") ? "bg-linear-to-br from-teal-200 to-purple-300 ml-7 self-start " 
+                : "bg-red-300 ml-7 text-red-900 font-bold"
+              ) + " max-w-100 min-w-50 mx-5 mb-5 rounded-md px-4 py-2 drop-shadow-md brightness-100 transition-all duration-150 ease-in-out hover:brightness-95"}>{msg.content}</div>
           ))
         }
         </div>
-        <div className='flex justify-between items-center bg-white w-full min-h-10 drop-shadow-sm rounded-lg px-5 py-1 outline-none mt-10'>
-          <TextareaAutosize
-            minRows={1}
-            maxRows={5}
-            placeholder='Type a message...'
-            className="flex-1 w-full overflow-y-hidden resize-none outline-0"
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={onMessageSend}
-            value={message}
-            disabled={isLoading}
-          />
+        <div className='flex justify-between items-center mb-5 mx-7'>
+          <div className={'flex flex-1 justify-between items-center  min-h-10 drop-shadow-sm rounded-md px-5 py-1 mr-2 outline-none transition-all ease-in-out duration-500  '+ (isLoading ? " bg-gray-100 cursor-wait" : " bg-white")}>
+            <TextareaAutosize
+              minRows={1}
+              maxRows={5}
+              placeholder='Type a message...'
+              className={"flex-1 w-full overflow-y-hidden resize-none outline-0 transition-all ease-in-out duration-500 " + (isLoading ? " bg-gray-100 cursor-wait" : " ")}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={onMessageSend}
+              value={message}
+              disabled={isLoading}
+            />
+          </div>
+          <div 
+            className='bg-gray-300 rounded-md opacity-70 p-2 w-20 text-center drop-shadow-md hover:opacity-90 hover:cursor-pointer'
+            onClick={() => {setMessages([]); setIsLoading(false);}}
+          >
+            Clear
+          </div>
         </div>
       </div>
+    </div>
   );
 }
  
