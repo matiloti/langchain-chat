357946a318b7f1117b3bbb5c8df9a969405c25f7
Matias <matiaslotitoralli@gmail.com>
2026-03-13 17:10:39 +0100

    no messages yet text

--- src/AppStreaming.tsx
+++ src/AppStreaming.tsx
@@ -170,7 +170,7 @@ function AppStreaming() {
       <div className='flex flex-col flex-1 justify-end w-full lg:max-w-200 md:max-w-190 bg-gray-50 drop-shadow-xl rounded-md min-h-0'>
         <div className='overflow-y-auto flex flex-col pt-5 h-full'>
         {
-          !messages || messages.length == 0 ? <div className='self-center mb-auto'>Makuta</div> : messages.filter(m => m.content.length > 0).map(msg => (
+          !messages || messages.length == 0 ? <div className='self-center flex-1 flex items-center justify-center text-gray-400'>No messages yet 🏃</div> : messages.filter(m => m.content.length > 0).map(msg => (
             <div className={
               (
                 (msg.role === "user") ? "bg-linear-to-br from-green-200 to-green-300 self-end " 
